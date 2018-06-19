11697f44b465453b1d05aaa3d71812d264443c1c
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-19 23:25:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(arr)
+  i = 0 
+  first_names = []
+  while i < arr.length 
+    first_names.push(yield(arr[i])) 
+    i+= 1 
+  end 
+  return first_names 
+end
+    
+arr = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(arr) { |name| name.split(" ").first}
